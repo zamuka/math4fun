ad640c643bff0083d6399e4fdc829405b0ba2b0d
Serhii Zamoshnikov <zamukan@gmail.com>
2022-09-20 16:44:04 +0200

    Moving sounds

--- public/sounds/index.ts
+++
@@ -1,8 +0,0 @@
-export const SOUND_EFFECTS = {
-  correct: '/assets/sounds/correct.mp3',
-  wrong: '/assets/sounds/wrong.mp3',
-  positiveDing: '/assets/sounds/positive-ding.mp3',
-  completeDing: '/assets/sounds/complete-ding.mp3',
-  click: '/assets/sounds/click1.mp3',
-  anticipationSlowed: '/assets/sounds/anticipation-slowed.mp3'
-};
